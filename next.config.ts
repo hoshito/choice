3f71cd8a643ba53030f41f7e3091edac224953bd
hoshito <10190139+hoshito@users.noreply.github.com>
2024-11-11 23:19:19 +0900

    job実行

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
     unoptimized: true  // GitHub Pagesでは画像最適化が使えないため
   },
   basePath: '/choice', // リポジトリ名を指定
+  assetPrefix: '/choice', // basePath と同じ値を設定
 };
 
 export default nextConfig;
