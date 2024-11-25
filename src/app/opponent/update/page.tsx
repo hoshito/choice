11a41e34b0040f9af508571f9e284ad30bcdc47a
hoshito <10190139+hoshito@users.noreply.github.com>
2024-11-25 05:33:03 +0000

    suspenseの導入

--- src/app/opponent/update/page.tsx
+++ src/app/opponent/update/page.tsx
@@ -1,11 +1,19 @@
 "use client";
 
+import { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { UpdateOpponentTeamClient } from './client';
 
-export default function UpdateOpponentTeamPage() {
+function UpdateForm() {
   const searchParams = useSearchParams();
   const teamId = searchParams.get('id');
-
   return <UpdateOpponentTeamClient teamId={teamId} />;
+}
+
+export default function UpdateOpponentTeamPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <UpdateForm />
+    </Suspense>
+  );
 } 
--- src/app/team/update/page.tsx
+++ src/app/team/update/page.tsx
@@ -1,11 +1,19 @@
 "use client";
 
+import { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { UpdateTeamClient } from './client';
 
-export default function UpdateTeamPage() {
+function UpdateForm() {
   const searchParams = useSearchParams();
   const teamId = searchParams.get('id');
-
   return <UpdateTeamClient teamId={teamId} />;
+}
+
+export default function UpdateTeamPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <UpdateForm />
+    </Suspense>
+  );
 }
