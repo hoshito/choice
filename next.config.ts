afff898afe8162a984ea90b55c726bb957d74be9
hoshito <10190139+hoshito@users.noreply.github.com>
2024-11-25 05:12:20 +0000

    動的エクスポート二変更

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',  // 静的エクスポートを有効化
   images: {
     unoptimized: true  // GitHub Pagesでは画像最適化が使えないため
   },
